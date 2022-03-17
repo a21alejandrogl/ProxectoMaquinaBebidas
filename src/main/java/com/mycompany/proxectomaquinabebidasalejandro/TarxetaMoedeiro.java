6dfa32364b9e468504bcf5b14936e666cb6889a6
a21alejandrogl <a21alejandrogl@iessanclemente.net>
2022-03-17 18:28:01 +0100

    Alejandro García Losas

---
+++ src/main/java/com/mycompany/proxectomaquinabebidasalejandro/TarxetaMoedeiro.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proxectomaquinabebidasalejandro;
+
+/**
+ *
+ * @author Alejandro García Losas
+ */
+public class TarxetaMoedeiro {
+    
+    private int saldo;
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public TarxetaMoedeiro(int saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
